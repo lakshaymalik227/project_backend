67bd64876c4eed8a4e55107466ed1df24c0288a2
lakshaymalik227 <lakshaymalik828@gmail.com>
2025-09-08 17:18:17 +0530

    add register controller and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //require('dotenv').config({path: './env'})
-
+import {app} from "./app.js"
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
@@ -10,7 +10,7 @@ dotenv.config({
 
 connectDB()
 .then(
-    app.lisen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log(` Server is running at port : ${process.env.PORT} `);
     })
 )
